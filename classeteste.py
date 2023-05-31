cade2a1a02a906a09e163132e1c816f798db07d7
puzzoviodev <silviopuzz@gmail.com>
2023-05-31 11:38:15 -0300

    estudo classes

---
+++ classeteste.py
@@ -0,0 +1,17 @@
+class DataTable:
+    def __init__(self,name):
+        self._name = name
+        sef._columns = []
+        sel.__data = []
+    def getData(self):
+        return self._data
+
+
+class Columm:
+    def __init__(self, name,kind,description):
+        self._name = name
+        self._kind = kind
+        self._description = description
+
+    table = DataTable("TabelaTeste")
+    print(table.getData())
