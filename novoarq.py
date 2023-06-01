de56fda3cd9315f9e14e3290b932f74e5e2bb353
Silvio <silviopuzz@gmail.com>
2023-06-01 11:02:29 -0300

    tetete del

--- novoarq.py
+++ novoarq.py
@@ -1,1 +1,16 @@
 print("arquivo adicionado")
+
+
+x = 5
+if x == 5:
+    y=0
+    while y <=10:
+        print("valor de y " , y)
+        y= y+1
+
+    for  i in [5,6,7,8,9]:
+        print(i)
+
+    for i in range(5):
+        print(1)
+
