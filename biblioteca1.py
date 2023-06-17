38540d6d6330908e679abe3af7a7b80078631785
puzzoviodev <silviopuzz@gmail.com>
2023-06-16 21:17:28 -0300

    numeros

---
+++ biblioteca1.py
@@ -0,0 +1,7 @@
+import math
+
+num = int(input("digite um numero  : "))
+
+raizquadrada = math.sqrt(num)
+
+print(f"Raiz quadrada de  {num} e   {raizquadrada} arendondado {math.ceil(raizquadrada)}")
