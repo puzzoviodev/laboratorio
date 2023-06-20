e7e527407435fd6ae8cd7c027c8bb9eb31257db4
Silvio <silviopuzz@gmail.com>
2023-06-20 09:17:13 -0300

    funcao

---
+++ aritimetica.py
@@ -0,0 +1,8 @@
+x = float(input("digite um numero 1 : "))
+y = float(input("digite um numero 2 : "))
+soma = x + y
+
+print("A soma :", soma)
+print("soma entre {} e  {} = {}".format(x,y,soma))
+print("soma entre {} e  {} = {}".format("a","b","c","F"))
+print(f"oma entre {x} e {y} = {soma}")
---
+++ rotina1.py
@@ -0,0 +1,12 @@
+a=10
+b=11
+def soma():
+    res = a+b
+    print(f" resultado da soma de a + b : {res}")
+def linha():
+
+    print(" ******************** "*20)
+
+linha()
+soma()
+linha()
