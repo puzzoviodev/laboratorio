5674f1aeae6dce59c8e23f4371b72e828f8d9634
Silvio <silviopuzz@gmail.com>
2023-06-21 09:37:20 -0300

    funcao

---
+++ rotina4.py
@@ -0,0 +1,24 @@
+def diferenca(a,b):
+    res = a-b
+    return res
+
+
+
+
+
+x=10
+y=20
+
+retorno1 = diferenca(x,y)
+
+print(f" retorno 1 {retorno1}")
+
+
+x=30
+y=20
+
+retorno2 = diferenca(x,y)
+
+print(f" retorno 2 {retorno2}, {x} - {y}")
+
+
