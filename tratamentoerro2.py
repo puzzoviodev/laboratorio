0c59bc4dd6aeca44d0058e960838edcb9b7e1529
Silvio <silviopuzz@gmail.com>
2023-06-21 11:27:36 -0300

    rotina de erro

--- tratamentoerro2.py
+++ tratamentoerro2.py
@@ -3,12 +3,11 @@ while True:
         n=int(input("digite um numerador : "))
         d=int(input("digite um denominador  : "))
         resultado = n/d
-    except:
-        print("erro ocorrido, reveja os valores")
+    except Exception as erro:
+        print(f"erro ocorrido {erro}")
     else:
         print(f"resultado {resultado}")
     finally:
         if input("deseja continuar ? s ou n  ") !="s":
             break
 
-a
