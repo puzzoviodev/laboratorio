76c31fa7f6f56c1d9e4ccc09bcc40b42a3723f2d
Silvio <silviopuzz@gmail.com>
2023-06-21 09:42:15 -0300

    funcao

--- rotina4.py
+++ rotina4.py
@@ -14,11 +14,16 @@ retorno1 = diferenca(x,y)
 print(f" retorno 1 {retorno1}")
 
 
-x=30
+a=30
 y=20
 
-retorno2 = diferenca(x,y)
+retorno2 = diferenca(a,y)
 
-print(f" retorno 2 {retorno2}, {x} - {y}")
+print(f" retorno 2 {retorno2}")
+xa=40
+xy=20
+print(f" retorno 3 {diferenca(xa,xy)}")
+
+
 
 
