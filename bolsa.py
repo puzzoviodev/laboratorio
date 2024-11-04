363faeb440a6d8a7666a738f7d138d385cce667e
puzzoviodev <80429983+puzzoviodev@users.noreply.github.com>
2024-11-04 15:43:36 -0300

    Update bolsa.py
    2

--- bolsa.py
+++ bolsa.py
@@ -1,4 +1,3 @@
-
 import openpyxl
 from openpyxl.styles import Font
 from openpyxl.styles import Font, PatternFill
@@ -10,12 +9,12 @@ from openpyxl.styles.differential import DifferentialStyle
 
 
 
-
 #IndValuation
 #IndEndividamento
 #IndiEficiência
 #IndiRentabilidade
 #IndiCrescimento
+#wb = openpyxl.load_workbook('statusinvest2.xlsx')
 wbsaida = openpyxl.Workbook()
 
 #cria planilhas
@@ -49,13 +48,26 @@ def criaCelulaIndiCrescimento(IndiCrescimento):
     IndiCrescimento = wbsaida['IndiCrescimento']
     IndiCrescimento.append(['CAGR Receitas 5 anos', 'CAGR Lucros 5 anos'])
     return
+
+def gravaCelulaIndiRentabilidade(imposto,celula):
+
+    #values = ws.cell(row=imposto, column=1).value
+    var1 = 'A' + str(imposto)
+    celula[var1] = 'teste1'
+    return
+
 criaPlanilaIndValuation(wbsaida)
 criaPCelulaIndEndividamento(wbsaida)
 criaCelulaIndiEficiência(wbsaida)
 criaCelulaIndiRentabilidade(wbsaida)
 criaCelulaIndiCrescimento(wbsaida)
 
+imposto = 1
+while imposto < 10:
+    imposto = imposto + 1
+    celula = wbsaida['IndiRentabilidade']
+    gravaCelulaIndiRentabilidade(imposto,celula)
 
-
+    print(imposto)
 
 wbsaida.save("exemplo2.xlsx")
