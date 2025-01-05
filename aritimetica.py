8b3d493e26457129a578eb578ece47b535f8eaad
silvi <silviopuzz@gmail.com>
2025-01-05 10:08:28 -0300

    aaa

--- aritimetica.py
+++ aritimetica.py
@@ -2,7 +2,7 @@ x = float(input("digite um numero 1 : "))
 y = float(input("digite um numero 2 : "))
 soma = x + y
 
-print("A soma 2:", soma)
+print("A soma w2:", soma)
 print("soma entre {} e  {} = {}".format(x,y,soma))
 print("soma entre {} e  {} = {}".format("a","b","c","F"))
 print(f"oma entre {x} e {y} = {soma}")
